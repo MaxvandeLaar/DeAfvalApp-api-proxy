936163692d6b3290d0702d7929e4ca0588f5dcce
MaxvandeLaar <max.vandelaar@me.com>
2018-01-04 19:10:18 +0100

    Improved test

--- test/address.js
+++ test/address.js
@@ -1,11 +1,11 @@
 process.env.NODE_ENV = 'test';
 
-let chai = require('chai');
-let beforeEach = require('mocha').before;
-let chaiHttp = require('chai-http');
-let Proxy = require('../deAfvalApp-api-proxy.js');
-let should = chai.should();
-let expect = chai.expect;
+const chai = require('chai');
+const beforeEach = require('mocha').before;
+const chaiHttp = require('chai-http');
+const Proxy = require('../deAfvalApp-api-proxy.js');
+const should = chai.should();
+const expect = chai.expect;
 
 chai.use(chaiHttp);
 
@@ -16,15 +16,8 @@ describe('Address', () => {
     });
     describe('#getAddress()', () => {
         it(`it should return the full address based on zip code & house number`, async () => {
-
             const address = await proxy.address.get("6118As", 14);
-
-            console.log("+++++");
-            console.log(address);
-            console.log("+++++");
-            address.should.have.property('zipcode');
-
-
+            address.should.have.property('success').equal(true);
         });
     });
 });
